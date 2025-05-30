f4719b2dea9e05d27b4d4b43dcf1fa900c1d3259
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-30 22:05:49 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BalanceProvider } from './context/BalanceContext';
-import { Analytics } from '@vercel/analytics/react'; // Import Analytics
+// import { Analytics } from '@vercel/analytics/react'; // Import Analytics
 import Navbar from './components/navbar';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -67,7 +67,7 @@ function App() {
       <BalanceProvider>
         <Router>
           <Layout />
-          <Analytics />
+          // <Analytics />
         </Router>
       </BalanceProvider>
     </QueryClientProvider>
